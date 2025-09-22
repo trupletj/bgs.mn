00de850a86a6f5810427d0f9dec171100f0f1dfa
trupletj <tuvshinjargal01@gmail.com>
2025-09-22 13:29:11 +0800

    eslint rules changed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,6 +19,22 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
+      "no-unused-vars": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "react/prop-types": "off",
+      "jsx-a11y/anchor-is-valid": "off",
+      "import/no-anonymous-default-export": "off",
+      "import/prefer-default-export": "off",
+      "unused-imports/no-unused-imports": "warn",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        { vars: "all", varsIgnorePattern: "^_", args: "after-used", argsIgnorePattern: "^_" },
+      ],
+     
+    },
   },
 ];
 
