5d5a889c729ee3ca4ad38a59f5a0a260e734fca7
trupletj <tuvshinjargal01@gmail.com>
2025-10-20 15:34:23 +0800

    change supabase publishable key

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -1,12 +1,15 @@
 import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
+
 export async function createClient() {
   const cookieStore = await cookies()
 
   return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+     supabaseUrl!,
+    supabaseKey!,
     {
       cookies: {
         getAll() {
